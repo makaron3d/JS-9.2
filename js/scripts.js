a379664fa693091461892d766ce7694cfb087fa7
makaron3d <makaron3d@gmail.com>
2017-08-14 17:41:35 +0200

    016

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var str = 'test';
+console.log(str);
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = maleNames.concat(femaleNames);
@@ -7,8 +9,25 @@ console.log(allNames);
 var newName = 'Maja';
 
 if (allNames.indexOf(newName) === -1) {
-    var newName = allNames.push(newName);
+    allNames.push(newName);
     alert(allNames);
     console.log(allNames);
 }
+
+console.log(allNames[3]);
+console.log(allNames.length);
+
+var txt = 'Example';
+
+console.log(txt.charAt(1));
+
+var motto = 'Good friends are loyal people';
+partOfMotto = motto.slice(-12,-5);
+
+console.log(partOfMotto);
+
+var motto = 'The rest is silence';
+var mottoCharsAfter = motto.substr(12, 8);
+
+console.log(mottoCharsAfter);
 
